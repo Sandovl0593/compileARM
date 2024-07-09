65e04a84d974597f321c82b24cce068d101a1c7d
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2024-07-09 09:07:13 -0500

    fixx

--- src/fmul16.v
+++ src/fmul16.v
@@ -2,40 +2,42 @@ module fmul16 (
     input wire [15:0] A,
     input wire [15:0] B,
     output wire [15:0] Result,
+    output wire [3:0] ALUFlags
 );
 
-    wire [4:0] exponentA, exponentB, exponentRes;
-    wire [9:0] mantissaA, mantissaB;
+    wire [3:0] exponentA, exponentB;
+    wire [4:0] exponentCur, exponentRes;
+    wire [8:0] mantissaA, mantissaB;
     wire sign;
-    wire [19:0] product;
+    wire [17:0] product;
     wire [9:0] mantissaNorm;
 
-    // Step 1: Extract exponent
-    assign exponentA[4] = 1'b0;
-    assign exponentA[3:0] = A[14:11];
-    assign exponentB[4] = 1'b0;
-    assign exponentB[3:0] = B[14:11];
+    assign exponentA = A[14:11];
+    assign mantissaA = {1'b1, A[10:1]};
 
-    // Step 2: Set mantissas
-    // in A and B, the first bit is always 1 (implicit) and the rest is the fraction
-    assign mantissaA[9] = 1'b1;
-    assign mantissaA[8:0] = A[10:1]; 
-    assign mantissaB[9] = 1'b1;
-    assign mantissaB[8:0] = B[10:1];
+    assign exponentB = B[14:11];
+    assign mantissaB = {1'b1, B[10:1]};
 
-    // Step 3: Calculate exponent
-    assign exponentRes = exponentA + exponentB - 15;
     assign sign = A[15] ^ B[15]; // Calculate sign
-
-    // Step 4: Multiply mantissas
+    // Calculate exponent
+    assign exponentCur = exponentA + exponentB - 15;
+    // Multiply mantissas
     assign product = mantissaA * mantissaB;
 
-    // Step 6: Normalize mantissa
-    // if the 20th bit is 1, then the result is norm
+    // Normalize mantissa
+    // if the 18th bit is 1, then the result is norm
     // otherwise the result needs to be shifted to the right
-    assign mantissaNorm = (product[19]) ? product[18:8] : product[17:7];
+    assign mantissaNorm = (product[17]) ? product[16:8] : product[15:7];
 
-    // Step 7: Combine the result
-    assign Result = {sign, exponentRes, mantissaNorm};
+    assign exponentRes = exponentCur + product[17];
+
+    // Combine the result
+    assign Result = {sign, exponentRes[4:0], mantissaNorm};
+
+    assign neg = (mantissaNorm[9] == 1);
+    assign carry = (product[19] == 1);
+    assign zero = (mantissaNorm == 10'h000000 && exponentRes == 5'h00);
+    assign overflow = (exponentRes > 5'h1F);
+    assign ALUFlags = {neg, zero, carry, overflow};
 
 endmodule
--- src/fmul32.v
+++ src/fmul32.v
@@ -2,40 +2,43 @@ module fmul32 (
     input wire [31:0] A,
     input wire [31:0] B,
     output wire [31:0] Result,
+    output wire [3:0] ALUFlags
 );
 
-    wire [8:0] exponentA, exponentB, exponentRes;
+    wire [7:0] exponentA, exponentB;
+    wire [8:0] exponentCur; exponentRes;
     wire [23:0] mantissaA, mantissaB;
     wire sign;
-    wire [47:0] product
+    wire [47:0] product;
     wire [22:0] mantissaNorm;
+    wire neg, zero, carry, overflow;
 
-    // Step 1: Extract exponent
-    assign exponentA[8] = 1'b0;
-    assign exponentA[7:0] = A[30:23];
-    assign exponentB[8] = 1'b0;
-    assign exponentB[7:0] = B[30:23];
+    assign exponentA = A[30:23];
+    assign mantissaA = {1'b1, A[22:0]};
 
-    // Step 2: Set mantissas
-    // in A and B, the first bit is always 1 (implicit) and the rest is the fraction
-    assign mantissaA[23] = 1'b1;
-    assign mantissaA[22:0] = A[22:0]; 
-    assign mantissaB[23] = 1'b1;
-    assign mantissaB[22:0] = B[22:0];
+    assign exponentB = B[30:23];
+    assign mantissaB = {1'b1, B[22:0]};
 
-    // Step 3: Calculate exponent
-    assign exponentRes = exponentA + exponentB - 127;
     assign sign = A[31] ^ B[31]; // Calculate sign
-
-    // Step 4: Multiply mantissas
+    // Calculate exponent
+    assign exponentCur = exponentA + exponentB - 127;
+    // Multiply mantissas
     assign product = mantissaA * mantissaB;
 
-    // Step 6: Normalize mantissa
+    // Normalize mantissa
     // if the 48th bit is 1, then the result is norm
     // otherwise the result needs to be shifted to the right
     assign mantissaNorm = (product[47]) ? product[46:24] : product[45:23];
 
-    // Step 7: Combine the result
-    assign Result = {sign, exponentRes, mantissaNorm};
+    assign exponentRes = exponentCur + product[47];
+
+    // Combine the result
+    assign Result = {sign, exponentRes[7:0], mantissaNorm};
+
+    assign neg = (mantissaNorm[22] == 1);
+    assign carry = (product[47] == 1);
+    assign zero = (mantissaNorm == 23'h0000000 && exponentCur == 8'h00);
+    assign overflow = (exponentCur > 8'hFF);
+    assign ALUFlags = {neg, zero, carry, overflow};
 
 endmodule
