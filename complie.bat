71afbed7c434fdfbadf7b0248ab41e056478512a
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2024-07-08 23:23:04 -0500

    with singlecycle

--- complie.bat
+++ complie.bat
@@ -6,4 +6,4 @@ python arm.py %1
 REM execute testbench.v with iverilog and vvp
 iverilog -o output src/testbench.v src/adder.v src/alu.v src/arm.v src/condcheck.v src/condlogic.v src/controller.v src/datapath.v src/decode.v src/dmem.v src/extend.v src/flopenr.v src/flopr.v src/imem.v src/mux2.v src/regfile.v src/top.v
 vvp output
-gtkwave test.vcd
+@REM gtkwave output.vcd
